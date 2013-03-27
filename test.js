16bc9c483259588f64d4641ea7cf0debbf85dcea
Max Ogden <max@maxogden.com>
2013-03-27 12:31:50 -0700

    add demo

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-var game = require('voxel-hello-world')()
+var textures = "http://commondatastorage.googleapis.com/voxeltextures/"
+var game = require('voxel-hello-world')({
+  texturePath: textures,
+  playerSkin: textures + 'player.png'
+})
 var fly = require('./')
 var makeFly = fly(game)
 makeFly(game.controls.target())
